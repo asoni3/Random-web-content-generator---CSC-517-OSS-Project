02e0c96b10c5c73534f79880411c452e07bac969
DileepBadveli <dileep.badveli@gmail.com>
2018-04-20 08:14:09 -0400

    take screenshots of 5 html files and save it

--- firefox_automation_screenshot.py
+++ firefox_automation_screenshot.py
@@ -1,6 +1,3 @@
-#Resolution for 1200x800 has to have dimension of
-#.set_window_size(1229,881)
-
 
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -10,9 +7,10 @@ import os
 
 
 browser = webdriver.Firefox(executable_path=r'C:\geckodriver-v0.20.1-win64\geckodriver.exe') # path to geckodriver.exe, to get instance of firefox
-file= os.path.abspath("file0.html")
-browser.get("file:///"+file) #go to html page withe firefox
-browser.set_window_size(1229,881) #set size to 1229x881(includes the border of browser) to get resolution 1200x800
-browser.save_screenshot("screen0.png") #saves the current screen
+for x in range(5):
+        file= os.path.abspath("file"+str(x)+".html")
+        browser.get("file:///"+file) #go to html page withe firefox
+        browser.set_window_size(1229,881) #set size to 1229x881(includes the border of browser) to get resolution 1200x800
+        browser.save_screenshot("screen"+str(x)+".png") #saves the current screen
 browser.close()
 
