78908fe2089fb5398467dbb9ca7f95c4a04503c7
Alexander <adsimps3@ncsu.edu>
2018-04-27 16:29:34 -0400

    Updates to code generation and added screenshot comparison file

--- code_generation.py
+++ code_generation.py
@@ -8,56 +8,150 @@ Created on Wed Mar 21 19:45:42 2018
 import random
 import string
 import os
+import sys
 
-#Short-listed some of the most commonly used fonts in web pages
-fonts = ['Arial','Helvetica','Times New Roman','Times','Courier New','Courier','Verdana','Georgia','Palatino','Garamond','Bookman','Comic Sans MS','Trebuchet MS','Arial Black','Impact']
-font_style = ['normal', 'italic','oblique']
-font_weight = ['normal', 'bold']
-font_align = ['center', 'left']
 html = ''
-css_contents = {}
-count=0
 css = ''
 
 #generate a skeleton HTML file with a doctype, head element, and body element and adds CSS to it
-def RandomHtml():
-    yield '<!DOCTYPE html>'
+def RandomHtml(max_depth, super_random_css, num_sibling_divs, tree_height, quirks_mode_possible, max_headers, min_headers):
+    count = [0]
+    max_depth = [max_depth]
+    
+    quirksMode = random.randint(0,1)
+    if quirksMode == 1 or quirks_mode_possible == False:
+        yield '<!DOCTYPE html>'
+        
     yield '<html>\r\n'
     yield ' RANDOMCSSCONTENTHERETOBEPLACED '
     yield '<body>\r\n'
-    yield RandomSection()
+    yield RandomSection(count, max_depth, num_sibling_divs, tree_height, max_headers, min_headers)
     yield '</body>\r\n</html>\r\n'
-    RandomCSS()
+    RandomCSS(count[0], super_random_css)
 
 #generate a random section
-def RandomSection():
+def RandomSection(count, max_depth, num_sibling_divs, tree_height, max_headers, min_headers):
     global css
-    global count
-    for x in range(0,random.randrange(5,10)):
+    css_contents = {}
+
+    for x in range(0,random.randrange(min_headers, max_headers)):
         random_header_number = random.randrange(3,5)
-        if not '<h'+str(random_header_number)+' id=a"'+str(count)+'">' in css_contents:
-            css_contents[count] = '<h'+str(random_header_number)+'>'
-            count+=1
-        yield '<p id="a'+str(count)+'">'
-        count+=1
+        if not '<h'+str(random_header_number)+' id=a"'+str(count[0])+'">' in css_contents:
+            css_contents[count[0]] = '<h'+str(random_header_number)+'>'
+            count[0]+=1
+        yield '<p id="a'+str(count[0])+'">'
+        count[0]+=1
         num_of_sentences = random.randrange(2, 15)
         for _ in range(num_of_sentences):
-             yield RandomSentence()
+            yield RandomSentence(count[0])
+        #Create a nested element 30% of the time
+        if random.random() < .3:
+            yield NestedElement(count, max_depth, num_sibling_divs, tree_height, max_headers, min_headers)
         yield '</p>\r'
         yield ('\n')
 
+def NestedElement(count, max_depth, num_sibling_divs, tree_height, max_headers, min_headers):
+    choose_element=[RandomSection, RandomTable, RandomOrderedList,RandomUnorderedList, RandomDiv]
+        
+    if max_depth[0] > 0:
+        max_depth[0] = max_depth[0] - 1  
+        chosen_element = random.choice(choose_element)
+        
+        if chosen_element == RandomDiv:
+            yield chosen_element(tree_height, count, max_depth, num_sibling_divs, max_headers, min_headers)
+        elif chosen_element == RandomSection:
+            yield chosen_element(count, max_depth, num_sibling_divs, tree_height, max_headers, min_headers)
+        else:
+            yield chosen_element()
+            
 #generate a random sentence using random range function
-def RandomSentence():
-    global count
+def RandomSentence(count):
     num_of_words = random.randrange(5, 20)
-    yield (' '.join(RandomWord() for _ in range(num_of_words)) + '.')
+    yield RandomWord()
+    for _ in range(num_of_words-1):
+        #1 in 20 words will be within a span
+        z= random.randrange(0,99)
+        if z<5:
+            yield ' '
+            yield ''.join(RandomSpan(count))
+        else:
+            yield ' '
+            yield ''.join(RandomWord())
+    yield '. '
+            
+    #yield (' '.join(RandomWord() for _ in range(num_of_words)) + '.')
 
 def RandomWord():
     chars = random.randrange(2, 10)
     return ''.join(random.choice(string.ascii_lowercase) for _ in range(chars))
 
-def RandomCSS():
-    global count, fonts, font_style, font_weight, css
+def RandomTable():
+    column_count = random.randrange(1,10)
+    row_count = random.randrange(2,20)
+    yield '<table>\r\n'
+    #generate table head
+    yield '\t<tr>\r\n\t\t'
+    for _ in range(column_count):
+        yield '<th>'
+        yield RandomWord()
+        yield '</th>\r\n'
+    yield '\t</tr>'
+    #fill in rows
+    for _ in range(row_count-1):
+        yield '\t<tr>\r\n\t\t'
+        for _ in range(column_count):
+            yield '<td>'
+            yield str(random.randrange(0,1000))
+            yield '</td>'
+        yield '\r\n\t</tr>\r\n'
+    yield '</table>\r\n'
+
+def RandomOrderedList():
+    list_length=random.randrange(1,20)
+    yield '<ol>\r\n'
+    for _ in range(list_length):
+        yield '\t<li>'
+        yield RandomWord()
+        yield '</li>\r\n'
+    yield '</ol>\r\n'
+
+def RandomUnorderedList():
+    list_length=random.randrange(1,20)
+    yield '<ul>\r\n'
+    for _ in range(list_length):
+        yield '\t<li>'
+        yield RandomWord()
+        yield '</li>\r\n'
+    yield '</ul>\r\n'
+
+def RandomDiv(tree_height, count, max_depth, num_sibling_divs, max_headers, min_headers):
+    numDivs = random.randint(1,num_sibling_divs)
+    for i in range(0, numDivs):
+        yield '<div id="a'+str(random.randrange(0,count[0]))+'">\r\n'
+        yield RandomSection(count, max_depth, num_sibling_divs, tree_height, max_headers, min_headers)
+        if tree_height > 0:
+            treeHeight = random.randint(0, tree_height)
+            if treeHeight > 0:
+                yield RandomDiv(treeHeight - 1, count, max_depth, num_sibling_divs, max_headers, min_headers)
+            
+        yield '</div>'
+
+def RandomSpan(count):
+    yield '<span id="a'+str(random.randrange(0,count))+'">'
+    yield RandomWord()
+    yield '</span>'
+    
+def RandomCSS(count, super_random_css):
+    global css
+    fonts = ['Arial','Helvetica','Times New Roman','Times','Courier New','Courier','Verdana','Georgia','Palatino','Garamond','Bookman','Comic Sans MS','Trebuchet MS','Arial Black','Impact']
+    font_style = ['normal', 'italic','oblique']
+    font_weight = ['normal', 'bold']
+    font_align = ['center', 'left']
+    displayTypes = ['none', 'inline', 'block', 'inline-block']
+    floatTypes = ['left', 'right', 'none', 'inherit']
+    borderTypes = ['dotted', 'dashed', 'solid', 'double', 'groove']
+
+    
     string_css = '<head>\r\n<style>\r\n'
     for x in range(count):
         r = lambda: random.randint(0,255)
@@ -67,8 +161,21 @@ def RandomCSS():
         string_css += 'font-size: '+str(random.randrange(10, 25)) +'px;\r\n'
         string_css += 'font-style: '+font_style[random.randrange(0, len(font_style))]+';\r\n'
         string_css += 'font-weight: '+font_weight[random.randrange(0, len(font_weight))]+';\r\n'
+        
+        if super_random_css:
+            string_css += 'display: ' + displayTypes[random.randint(0, 3)] + ';\r\n'
+            string_css += 'background-color: rgb(' + str(random.randint(0, 255)) + ',' + str(random.randint(0, 255)) + ',' + str(random.randint(0, 255)) +');\r\n'
+            string_css += 'float: ' + floatTypes[random.randint(0, 3)] + ';\r\n'
+            string_css += 'padding: ' + str(random.randint(0, 20)) + 'px ' + str(random.randint(0, 20)) + 'px ' + str(random.randint(0, 20)) + 'px ' + str(random.randint(0, 20)) + 'px;\r\n'
+            string_css += 'margin: ' + str(random.randint(0, 20)) + 'px ' + str(random.randint(0, 20)) + 'px ' + str(random.randint(0, 20)) + 'px ' + str(random.randint(0, 20)) + 'px;\r\n'
+            string_css += 'border-width: ' + str(random.randint(0, 20)) + 'px ' + str(random.randint(0, 20)) + 'px ' + str(random.randint(0, 20)) + 'px ' + str(random.randint(0, 20)) + 'px;\r\n'
+            string_css += 'border-style: ' + borderTypes[random.randint(0, 4)] + ';\r\n'
+        
         string_css += 'text-align: '+font_align[random.randrange(0, len(font_align))]+';\r\n}\r\n'
+         
+    string_css += 'table,td,tr {border: 1px solid black;}'
     string_css += '\r\n</style>\r\n</head>\r\n'
+
     css = string_css
 
 def Output(generator):
@@ -77,16 +184,54 @@ def Output(generator):
         html = html+generator
     else:
         for g in generator: Output(g)
+    
+html = ''
+css = ''
 
-for x in range(0,5):
-    html = ''
-    css_contents = {}
-    count = 0
-    css = ''
-    Output(RandomHtml())
-    #print(css)
-    html = html.replace('RANDOMCSSCONTENTHERETOBEPLACED', css)
-    html_file = open("file{}.html".format(x), "w")
-    html_file.write(html)
-    html_file.close()
-    print("The file: html{}.hmtl created!".format(x))
+def main():
+    global html, css
+    
+    if len(sys.argv) == 3:  
+        simple = (sys.argv[1]).lower()
+        size = (sys.argv[2]).lower()
+        
+        if ((simple == 'true' or simple == 'false') and (size == 'small' or size == 'medium' or size == 'large')):
+            if simple == 'false':
+                super_random_css = False
+                depth = 1
+                num_sibling_divs = 1
+                max_tree_height = 0
+                quirks_mode_possible = False
+            elif simple == 'true':
+                super_random_css = True
+                depth = 3
+                num_sibling_divs = 4
+                max_tree_height = 3
+                quirks_mode_possible = True
+                
+            if size == 'small':
+                print(size)
+                max_headers = 4
+                min_headers = 2
+            elif size == 'medium':
+                max_headers = 11
+                min_headers = 8
+            elif size == 'large':
+                max_headers = 18
+                min_headers = 15
+            
+            Output(RandomHtml(depth, super_random_css, num_sibling_divs, max_tree_height, quirks_mode_possible, max_headers, min_headers))
+            html = html.replace('RANDOMCSSCONTENTHERETOBEPLACED', css)
+            
+            #printing to screen as suggested by reviewers
+            #output can now be easily saved as whatever name the user wants or easily output it to another tool
+            print(html)
+        else:
+            print('Please input the correct arguments: simple(\'true\' or \'false\') and size(\'small\', \'medium\', \'large\')')
+    else:
+        print('Please input the correct number of arguments: simple(\'true\' or \'false\') and size(\'small\', \'medium\', \'large\')')
+
+
+if __name__ == "__main__":
+    main()
+ 
---
+++ compare.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 22 10:45:26 2018
+
+@author: Alexander
+"""
+
+#must install opencv, imutils, and scikit-image
+
+from skimage.measure import compare_ssim 
+import imutils
+import cv2
+import numpy as np
+import sys
+from pathlib import Path
+
+def find_if_close(cnt1,cnt2):
+    row1,row2 = cnt1.shape[0],cnt2.shape[0]
+    for i in range(row1):
+        for j in range(row2):
+            dist = np.linalg.norm(cnt1[i]-cnt2[j])
+            if abs(dist) < 50 :
+                return True
+            elif i==row1-1 and j==row2-1:
+                return False
+
+def compareImages(comparison_local_path, comparison_name, image1_local_path_and_name, image2_local_path_and_name):            
+    image1 = image1_local_path_and_name
+    image2 = image2_local_path_and_name
+    
+    file1 = Path(image1)
+    file2 = Path(image2)
+    output_path = Path(comparison_local_path)
+
+    if not file1.exists():
+        print('Image 1 does not exist')
+    elif not file2.exists():
+        print('Image 2 does not exist')
+    elif not output_path.exists() and comparison_local_path != 'none' :
+        print(comparison_local_path)
+        print('Output Path does not exist')
+    else:              
+        imageA = cv2.imread(image1)
+        imageB = cv2.imread(image2)
+        
+        if len(imageA) != len(imageB) or len(imageA[0]) != len(imageB[0]):
+            print('Image Sizes don\'t match...')
+        else:
+            grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+            grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+            
+            (score, diff) = compare_ssim(grayA, grayB, full=True)
+            diff = (diff * 255).astype("uint8")
+        
+            print("Structural Similarity Index(SSIM): {}".format(score))
+            
+            thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+            cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+            
+            md = 50
+            rectangles = []
+            for c in cnts:
+                (x, y, w, h) = cv2.boundingRect(c)
+                addTo = True
+                
+                for r in rectangles:
+                    checker = False
+                    if (x) >= (r[0] - md) and (x) <= (r[0] + r[2] + md) and (y) >= (r[1] - md) and (y) <= (r[1] + r[3] + md):
+                        checker = True
+                    elif (x) >= (r[0] - md) and (x) <= (r[0] + r[2] + md) and (y + h) >= (r[1] - md) and (y + h) <= (r[1] + r[3] + md):
+                        checker = True
+                    elif (x + w) >= (r[0] - md) and (x + w) <= (r[0] + r[2] + md) and (y) >= (r[1] - md) and (y) <= (r[1] + r[3] + md):
+                        checker = True
+                    elif (x + w) >= (r[0] - md) and (x + w) <= (r[0] + r[2] + md) and (y + h) >= (r[1] - md) and (y + h) <= (r[1] + r[3] + md):
+                        checker = True
+                    
+                    if checker:
+                        #merge
+                        xMax = max((x + w),(r[0] + r[2]))
+                        yMax = max((y + h),(r[1] + r[3]))
+                        
+                        r[0] = min(r[0], x)
+                        r[1] = min(r[1], y)  
+                        r[2] = xMax - r[0]
+                        r[3] = yMax - r[1]
+                        addTo = False
+                        break
+                if addTo:
+                    rectangles.append([x, y, w, h])
+                        
+            changes = True
+            newRectangles = []
+            while changes:
+                changes = False
+                
+                for rrr in rectangles:
+                    x = rrr[0]
+                    y = rrr[1]
+                    w = rrr[2]
+                    h = rrr[3]
+                    
+                    addTo = True
+                    for r in newRectangles:
+                        checker = False
+                        if (x) >= (r[0] - md) and (x) <= (r[0] + r[2] + md) and (y) >= (r[1] - md) and (y) <= (r[1] + r[3] + md):
+                            checker = True
+                        elif (x) >= (r[0] - md) and (x) <= (r[0] + r[2] + md) and (y + h) >= (r[1] - md) and (y + h) <= (r[1] + r[3] + md):
+                            checker = True
+                        elif (x + w) >= (r[0] - md) and (x + w) <= (r[0] + r[2] + md) and (y) >= (r[1] - md) and (y) <= (r[1] + r[3] + md):
+                            checker = True
+                        elif (x + w) >= (r[0] - md) and (x + w) <= (r[0] + r[2] + md) and (y + h) >= (r[1] - md) and (y + h) <= (r[1] + r[3] + md):
+                            checker = True
+                        
+                        if checker:
+                            #merge
+                            xMax = max((x + w),(r[0] + r[2]))
+                            yMax = max((y + h),(r[1] + r[3]))
+                            
+                            r[0] = min(r[0], x)
+                            r[1] = min(r[1], y)  
+                            r[2] = xMax - r[0]
+                            r[3] = yMax - r[1]
+                            addTo = False
+                            changes = True
+                            break
+                    if addTo:
+                        newRectangles.append([x, y, w, h])
+                
+                rectangles = newRectangles[:]
+                newRectangles = []
+                
+                rectangles.reverse()
+                for rrr in rectangles:
+                    x = rrr[0]
+                    y = rrr[1]
+                    w = rrr[2]
+                    h = rrr[3]
+                    
+                    addTo = True
+                    for r in newRectangles:
+                        checker = False
+                        if (x) >= (r[0] - md) and (x) <= (r[0] + r[2] + md) and (y) >= (r[1] - md) and (y) <= (r[1] + r[3] + md):
+                            checker = True
+                        elif (x) >= (r[0] - md) and (x) <= (r[0] + r[2] + md) and (y + h) >= (r[1] - md) and (y + h) <= (r[1] + r[3] + md):
+                            checker = True
+                        elif (x + w) >= (r[0] - md) and (x + w) <= (r[0] + r[2] + md) and (y) >= (r[1] - md) and (y) <= (r[1] + r[3] + md):
+                            checker = True
+                        elif (x + w) >= (r[0] - md) and (x + w) <= (r[0] + r[2] + md) and (y + h) >= (r[1] - md) and (y + h) <= (r[1] + r[3] + md):
+                            checker = True
+                        
+                        if checker:
+                            #merge
+                            xMax = max((x + w),(r[0] + r[2]))
+                            yMax = max((y + h),(r[1] + r[3]))
+                            
+                            r[0] = min(r[0], x)
+                            r[1] = min(r[1], y)  
+                            r[2] = xMax - r[0]
+                            r[3] = yMax - r[1]
+                            addTo = False
+                            changes = True
+                            break
+                    if addTo:
+                        newRectangles.append([x, y, w, h])
+                
+                rectangles = newRectangles[:]
+                newRectangles = []
+                
+            for rrr in rectangles:
+                x = rrr[0]
+                y = rrr[1]
+                w = rrr[2]
+                h = rrr[3]
+                
+                cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            
+            if score != 1.0:
+                checkExtension = comparison_name.find('.')
+                if checkExtension > -1:
+                    comparison_name = comparison_name[:checkExtension]
+                    
+                if comparison_local_path != 'none':
+                    filename = comparison_local_path + comparison_name + '.png'
+                else:
+                    filename = comparison_name + '.png'
+                    
+                try:
+                    cv2.imwrite(filename,imageA)
+                except:
+                    print('Cannot write comparison file...')
+
+def main():
+    if len(sys.argv) == 5:  
+        output_path = sys.argv[1]
+        if output_path.lower() == 'none':
+            output_path = output_path.lower()
+        
+        output_name = sys.argv[2]
+        f1 = sys.argv[3]
+        f2 = sys.argv[4]
+        
+        if output_path[-1] != '/' and output_path != 'none':
+            output_path = output_path + '/'
+    
+        compareImages(output_path, output_name, f1, f2)  
+
+    else:
+        print('Please input the correct number of arguments: output path (or \'none\' if no path), output filename, image1 - local path + name (with extension), image2 local path + name (with extension)')
+
+if __name__ == "__main__":
+    main()
