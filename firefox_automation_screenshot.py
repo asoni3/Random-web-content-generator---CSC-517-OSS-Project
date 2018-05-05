9f27e050feed4c8d2dc9e4d685f9026edc9b9720
Abhay Soni <asoni3@ncsu.edu>
2018-05-05 09:34:58 -0400

    Modified to take viewport screenshot

--- firefox_automation_screenshot.py
+++ firefox_automation_screenshot.py
@@ -28,28 +28,17 @@ def main(argv):
             global num_of_files
             num_of_files = int(arg)
     browser = webdriver.Firefox()  # path to geckodriver
+    dx, dy = browser.execute_script("var w=window; return [w.outerWidth - w.innerWidth, w.outerHeight - w.innerHeight];")
+    browser.set_window_size(width + dx, height + dy)
     for x in range(num_of_files):
         file = os.path.abspath("file" + str(x) + ".html")
-        browser.get("file:///" + file)  # go to html page withe firefox
-        set_viewport(browser, width, height,x)
+        browser.get("file:///" + file)  # go to html page with firefox
         browser.save_screenshot("screen" + str(x) + ".png")  # saves the current screen
     browser.close()
 
-
-def set_viewport(driver, width, height,num_file):
-    driver.set_window_position(0,0)
-    driver.set_window_size(width,height)
-    driver.save_screenshot("screen" + str(num_file) + ".png")
-    img= Image.open("screen" + str(num_file) + ".png")
-    img_width, img_height=img.size
-    if img_width!=width:
-        width=width-(img_width-width)#correction to width length
-    if img_height!=height:
-        height=height-(img_height-height) #correction to height length
-    driver.set_window_size(width,height)
     
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 6:
         print('Argument input is required in the format')
         print(
             'python3 etc/servo_automation_screenshot.py -w <width> -h <height> -n <numOfFiles>')
